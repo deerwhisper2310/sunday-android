060266eea096068e0222de7ab3a0c7ed3fdd8df4
deerwhisper2310 <deerwhisper2310@pm.me>
2025-09-02 13:24:42 -0400

    update versionCode and versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.block.goose.sunday"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
