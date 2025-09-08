ebd4314438d866895eae079a323be9d7bf57cacf
deerwhisper2310 <deerwhisper2310@pm.me>
2025-09-07 21:27:51 -0400

    Update app version to 0.3.1
    This commit updates the `versionCode` to 5 and `versionName` to "0.3.1" in the `app/build.gradle.kts` file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.block.goose.sunday"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
